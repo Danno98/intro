a42b241ceb16177d0decc7d1a69f4744902e0b09
Danno98 <dannygeles@gmail.com>
2023-05-02 19:59:38 -0600

    Initial commit

---
+++ src/main/java/ico/fes/intro/model/Empleado.java
@@ -0,0 +1,18 @@
+package ico.fes.intro.model;
+
+import lombok.*;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Empleado {
+    private int id;
+    private double sueldo;
+    private String departamento;
+    private String nombre;
+    private String fechaNacimiento;
+}
